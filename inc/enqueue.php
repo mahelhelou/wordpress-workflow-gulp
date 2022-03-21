be0b716a58085fe9e8a7e9fef910724ec3375dac
mahelhelou <mahelhelou@gmail.com>
2022-03-22 01:32:04 +0200

    Updating index.pug and main.scss

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -11,7 +11,7 @@ function package_name_assets() {
   wp_enqueue_style( 'package-name-styles', get_template_directory_uri() . '/app/dist/style.css', [], THEME_VERSION );
 
   // Load scripts
-  wp_enqueue_script( 'oktobly-scripts', get_template_directory_uri() . '/app/dist/bundle.js', [], THEME_VERSION, true );
+  wp_enqueue_script( 'package-name-scripts', get_template_directory_uri() . '/app/dist/bundle.js', [], THEME_VERSION, true );
 }
 
 add_action( 'wp_enqueue_scripts', 'package_name_assets' );
