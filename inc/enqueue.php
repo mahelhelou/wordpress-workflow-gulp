d4176404ef506930d7678f94c3663446dd802b8b
Mahmoud El Helou <mahelhelou@gmail.com>
2022-03-25 21:12:41 +0200

    Editing file names and adding more files

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -8,10 +8,10 @@
 
 function package_name_assets() {
   // Load styles
-  wp_enqueue_style( 'package-name-styles', get_template_directory_uri() . '/app/dist/style.css', [], THEME_VERSION );
+  wp_enqueue_style( 'package-name-styles', get_template_directory_uri() . '/app/dist/styles.css', [], THEME_VERSION );
 
   // Load scripts
-  wp_enqueue_script( 'package-name-scripts', get_template_directory_uri() . '/app/dist/bundle.js', [], THEME_VERSION, true );
+  wp_enqueue_script( 'package-name-scripts', get_template_directory_uri() . '/app/dist/bundled.js', [], THEME_VERSION, true );
 }
 
 add_action( 'wp_enqueue_scripts', 'package_name_assets' );
